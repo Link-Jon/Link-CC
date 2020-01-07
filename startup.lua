752b9d69d2e822030527aaf76b2f47f28e59a8ae
Link-Jon <Lilmanjon8@aol.com>
2020-01-07 00:11:33 -0500

    moved the monitor stuff.
    to mtext

--- startup.lua
+++ startup.lua
@@ -2,45 +2,6 @@
 mon=peripheral.find("monitor")
 perp=false --Immibis peripherals  (gets checked with server)
 loop=0
-function mprint(str)
-    if monitors==true then
-            loop=loop+1
-            local x,y =mon.getCursorPos()
-            y=y+1
-            if loop==5 then
-                mon.scroll(5)
-                y=1
-                loop=0
-            end
-            mon.setCursorPos(1,y)
-            mon.write("\n"..str.."\n")
-            print(str)
-            return
-        else
-            print(str)
-            return
-    end
-end
-
-function mwrite(str)
-    if monitors==true then
-            loop=loop+1
-            local x,y=mon.getCursorPos()
-            y=y+1
-            if loop==5 then
-                mon.scroll(5)
-                y=1
-                loop=0
-            end
-            mon.setCursorPos(1,y)
-            mon.write(str)
-            write(str)
-            return
-        else
-            write(str)
-            return
-    end
-end
 
 exitvar=true --just incase i may ever use it...
 function Main()

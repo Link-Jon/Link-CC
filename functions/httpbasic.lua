fb0bedb8f7c9c644eac593c723ff44f54e45e50f
Link-Jon <Lilmanjon8@aol.com>
2020-01-07 14:18:29 -0500

    Update httpbasic.lua

--- functions/httpbasic.lua
+++ functions/httpbasic.lua
@@ -13,16 +13,27 @@ function github(arg[1],arg[2],arg[3],arg[4],arg[5],arg[6]) --note. lua arrays st
     nilcheck(arg[5],"master")
     nilcheck(arg[3],"Link-Jon")
     nilcheck(arg[6],"w")
+    --ugh. translate \/ that into a single var... using string api more than likely. so i can test it XD
+      ("https://raw.githubusercontent.com/"..arg[3].."/"..arg[4].."/"..arg[5].."/"..arg[2])
     if fs.getDir(arg[1]) or arg[6]=="r" then
-        local file = http.get("https://raw.githubusercontent.com/"..arg[3].."/"..arg[4].."/"..arg[5].."/"..arg[2])
+        http.getResponse()
+        local file = http.get(url)
         local f2 = file.readAll() -- all i need is the data. dont need the other stuff from the get request. or have anything else in the request either.
         if arg[6]=="W" then
-            file.close()
-            file.open(arg[1],"w")
-            file.write(f2)
-            file.close()
-        elseif arg[6]=="r"
-            mprint(f2)
-    else
-        return(false)
+                file.close()
+                file.open(arg[1],"w")
+                file.write(f2)
+                file.close()
+            elseif arg[6]=="r"
+                mprint(f2)
+        end
+        else
+            return(false)
+    end
+end
+
+function htest(url) -- very basic. will change.
+    
+    return(http.getResponse(url))
+    
 end

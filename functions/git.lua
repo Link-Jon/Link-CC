804f24a0778734a73f23921e0c4cb8548859a6d0
Link-Jon <Lilmanjon8@aol.com>
2020-01-07 09:50:56 -0500

    Error catching for updater.

--- functions/git.lua
+++ functions/git.lua
@@ -1,8 +1,13 @@
-function github(branch,name,filename)
-    branch==master -- better failsafe for errors pls...
-    if not filename then
-        local file = http.get("https://github.com/Link-Jon/CC-OSish/"..branch.."somethinggoeshere"..name)
-        local f2 file.readAll()
+--arg1==filename/filepath on the CC computer, 2==name/path on github, 3==user on github, 4==repo, 5==branch.
+--required: arg1 and arg2. if you put nil for 3, 4, or 5 it will use default.
+function github(arg[1],arg[2],arg[3],arg[4],arg[5]) --note. lua arrays start at 1, not 0. (changeable, but i'd rather not)
+    nilcheck(repo,"CC-OSish")
+    nilcheck(branch,"master")
+    nilcheck(user,"Link-Jon")
+    if not fs.getDir(filename) then
+        local file = http.get("https://raw.githubusercontent.com/Link-Jon/CC-OSish/"..branch.."/"..name)
+        local f2 file.readAll() -- all i need is the data. dont need the other stuff from the get request. or have anything else in the request either.
+        Link-Jon-1.0/functions/git.lua
         file.close()
         file.open(filename,"w")
         file.write(f2)
@@ -10,7 +15,3 @@ function github(branch,name,filename)
     else
         return(false)
 end
-
-
-
-  

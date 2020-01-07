49e11770d7aaf8e174683dfa4617d5c589ff1cd1
Link-Jon <Lilmanjon8@aol.com>
2020-01-07 01:02:18 -0500

    Github link
    created a rudimentary github file downloader. should be added to, for error catching. and throwing.

---
+++ functions/git.lua
@@ -0,0 +1,16 @@
+function github(branch,name,filename)
+    branch==master -- better failsafe for errors pls...
+    if not pathname then
+        local file = http.get("https://github.com/Link-Jon/CC-OSish/"..branch.."somethinggoeshere"..name)
+        local f2 file.readAll()
+        file.close()
+        file.open(filename,"w")
+        file.write(f2)
+        file.close()
+    else
+        return(false)
+end
+
+
+
+  

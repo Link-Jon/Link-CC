dbbc94ad26b4596c73078e6ceef603d1900d5c44
Link-Jon <Lilmanjon8@aol.com>
2020-01-07 09:27:58 -0500

    Bug fix
    if not pathname then
    changed to
    if not filename then 
    (line 3)
    as, pathname==nil

--- functions/git.lua
+++ functions/git.lua
@@ -1,6 +1,6 @@
 function github(branch,name,filename)
     branch==master -- better failsafe for errors pls...
-    if not pathname then
+    if not filename then
         local file = http.get("https://github.com/Link-Jon/CC-OSish/"..branch.."somethinggoeshere"..name)
         local f2 file.readAll()
         file.close()

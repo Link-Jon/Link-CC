e1c4f5284f82116b6d2ada85eb681e9f14620593
Link-Jon <Lilmanjon8@aol.com>
2020-09-09 11:20:15 -0400

    INSTALL FIXED!
    i feel like an idoit, i tried to write down the file handler instead of what the file handler read. WOOPS.

--- install.lua
+++ install.lua
@@ -15,18 +15,27 @@ local exception = {"startup.lua"} -- doesnt go into the directory. stays in root
 local counter=1
 
 if not fs.exists("sys") then
-        dir = "sys"
-        dircheck = true
+        local dir = "sys"
+        local dircheck = true
 end
         
 while not dircheck do
-    print("Input a brief version of your username, or the directory that you would like to use. This will contain pretty much all of my files. If it's not empty, then items inside may end up being deleted. This only appears after 'sys' has been checked (this will be the name for a directory)")
+    print("Input a name for the directory that you would like to use. This will contain pretty much all of my files. If it's not empty, then items inside may end up being deleted. This only appears after 'sys' has been checked (this will be the name for a directory)")
     dir = read()
     if not fs.exists(dir) then
             dircheck=true
         else
             dircheck=false
-            print("ERR: File/Folder exists")
+            write("WARNING: File/Folder exists. Continue? (y/n)")
+            local input = read()
+            if input=='y' then
+                dircheck=true
+            end
+        
+    end
+--Tell user that it will loop, if it is going to.
+    if dircheck==false then
+        print("Warning, this will loop until a usable directory has been found")
     end
 end
 --im going to let the print command have fun with that. infact ima go see how that works so i can
@@ -40,26 +49,30 @@ if dir then
     temp=nil
 end
 
-while counter do
+for counter=1,table.getn(gitlist)
     lista = gitlist[counter]
     listb = gotlist[counter]
-    
+    if lisa==nil then
+        break
+    end
+
     temp = http.get("https://raw.githubusercontent.com/Link-Jon/CC-OSish/Link-Jon-1.0/"..lista)
     --temp = http.get("https://raw.githubusercontent.com/Link-Jon/CC-OSish/master/"..lista)
     
-    temp.readAll()
+    raw=temp.readAll()
     if lista==exception[counter] or dir==nil then
             file = fs.open(listb,"w")
         else
             file = fs.open(dir.."/"..listb,"w")
     end
-    file.write(temp)
+    file.write(raw)
     file.close()
     print("Downloaded: "..lista.." as "..listb)
     counter=counter+1
     --bad. im going to let it bios error to end. not a great idea, should be changed.
+    --changed to For loop, fixes this err.
 end
-
+print("Finsihed!")
 end --ends function
 
 install()

cfd76e85b939da8ab4ea45130339b8d2d09cbcf7
JC <Link_jon@hotmail.com>
2026-01-11 11:58:41 -0500

    Moved hidden chat functionality to chatter.lua
    Fixed section sign message insertion (probably)
    
    Changed both hidden and terminal chat to be active at the same time via the parallel.api
    
    (Also made a dummy function for seeing what was sent ingame in the term. May need to log the chat to scroll it. ironiclly.
    Though this one can delete the old log, maybe?
    ill just make it a config setting... maybe)

--- programs/chatter.lua
+++ programs/chatter.lua
@@ -1,10 +1,3 @@
---Todo
---see chatLogger.lua
-
---Stylized for "play.dw20.ca"
---Requires Advanced Peripherals
---(Needs a chat box)
-
 --Link~
 
 args = {...}
@@ -19,28 +12,80 @@ local username = args[1]
 local prefix = args[2]
 local chat = peripheral.find("chatBox")
 
-while true do
-        
+
+function sectionSignHandling(msg)
+   --First: prevent 'escaped' characters from being changed
+   msg = string.gsub(msg, "%\%$", "__DOLLARSIGN__")
+   msg = string.gsub(msg, "%\%&", "__AMPERSAND__")
+
+   --Second: Replace non 'escaped' characters with the section sign (§)
+   msg = string.gsub(msg, "%$", "§")
+   msg = string.gsub(msg, "%&", "§")
+
+   --Third: Return 'escaped' characters to normal
+   msg = string.gsub(msg, "__DOLLARSIGN__", "$")
+   msg = string.gsub(msg, "__AMPERSAND__", "&")
+
+   return msg
+end
+
+--Chat functionality from inside the computer
+--probably a pocket computer.
+function computerChatter()        
     write("> ")
     msg = io.read()        
     --This should be easier to read.
-    if msg == "exit" then
-        break
-    elseif msg == "edit" then
-        shell.openTab("edit chatter.lua")
-        break
-    end
+   if msg == "exit" then
+      break
+   elseif msg == "edit" then
+      shell.openTab("edit chatter.lua")
+      break
+   end
+   
+   msg = sectionSignHandler(msg)
     
-    msg = string.gsub(msg,"%&","§")
-    msg = string.gsub(msg,"%$","§")
-    print(msg)
-    
-    premsg = "§7- "..username.."§f: "
-    if prefix == nil then
-        prefix = "§6CraftOS§r"
-    end
-    brackets = "[]" --brackets
-    bracketColor = "&8" --'bracketcolor', same as normal color codes, but with &
+   premsg = "§7- "..username.."§f: "
+   if prefix == nil then
+      prefix = "§6CraftOS§r"
+   end
+   brackets = "[]" --brackets
+   bracketColor = "&8" --'bracketcolor', same as normal color codes, but with &
         
-    chat.sendMessage(premsg..msg, prefix, brackets, bracketColor)
+   chat.sendMessage(premsg..msg, prefix, brackets, bracketColor)
+end
+
+--chat functionality from within chat;
+--via '$' hidden messages
+function hiddenChatter()
+
+   --todo: Unify the 'preset' between the two.
+   premsg = "§7- "..username.."§f: "
+   msg = msg
+   prefix = "§6CraftOS§r"
+   brackets  = "[]"
+   bracketColor = "§8"
+
+   msg = sectionSignHandler(msg)
+   
+   --All together:
+   chat.sendMessage(premsg..msg, prefix, brackets, bracketColor)
+end
+
+function standardChat()
+   --Show chat in terminal.
+   while true do
+      sleep(0)
+   end
+end
+
+--------
+
+while true do
+   parallel.waitForAny(
+      computerChatter,
+      hiddenChatter,
+      standardChat
+   )
+   
+   sleep()
 end
